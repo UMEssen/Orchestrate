5bf158ec251fef50622df342845f63a73f32fdf8
wen0705 <wenyutong7@gmail.com>
2026-09-14 17:38:23 +0200

    update

---
+++ utils/labels.py
@@ -0,0 +1,17 @@
+body_region_labels = ["head", 
+                      "abdominal_region", 
+                      "thoracic_region", 
+                      "pericardium"]
+body_organ_labels =[
+                "lung",
+                "heart",
+                "spine",
+                "liver",
+                "kidneys",
+                "spleen",
+                "stomach",
+                "colon",
+                "pancreas",
+                "brain",
+                "hip"
+            ]
